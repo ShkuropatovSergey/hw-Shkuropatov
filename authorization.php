af063d91bc43590e8c9914d65b77416965128517
ShkuropatovSergey <58105224+ShkuropatovSergey@users.noreply.github.com>
2023-04-24 12:45:32 +0300

    Добавил редирект на страницу регистрации

--- authorization.php
+++ authorization.php
@@ -1,14 +1,18 @@
 <?php
+
     $template = [
         'message' => null
-    ];
+    ];$s=0;
     if( isset($_POST['login']) ){
         if(( $_POST['login']=='admin1' and $_POST['password'] ==  'password1')
         or ( $_POST['login']=='admin2' and $_POST['password'] ==  'password2')
         or ( $_POST['login']=='admin3' and $_POST['password'] ==  'password3')){
             $template['message'] = 'Вы успешно авторизовались';
+            $s=1;
         }else{
-            $template['message'] = 'Логин или пароль введены неверно';    
+            $template['message'] = 'Логин или пароль введены неверно.<br>
+             Вы будете направлены на страницу регистрации.';
+            $s=2;   
         }
     }
 ?>
@@ -23,15 +27,22 @@
 </head>
 <body>
     <?php 
-        if( !empty( $template['message'] ) ){
+        if( !empty( $template['message'] ) && ($s==1)){
             echo "<h2>{$template['message']}</h2>";
+        }elseif( !empty( $template['message'] ) && ($s==2)){
+            echo "<h2>{$template['message']}</h2>";
+        ?> <script>setTimeout(function(){
+            window.location.href = 'registration.php';
+          }, 3000);</script><?
         }
     ?>
     <h1>Авторизация</h1>
-    <form method='post'>
+    <form class="aa" method='post'>
         <input type="text" name='login' placeholder='Логин'>
         <input type="password" name='password' placeholder='Пароль'>
         <button type="submit">Авторизоваться</button>
     </form>
+   
+   
 </body>
 </html>
