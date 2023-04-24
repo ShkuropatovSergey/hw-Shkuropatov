0aa9554850b23259d1ef3e203a07f371ccf977e7
ShkuropatovSergey <58105224+ShkuropatovSergey@users.noreply.github.com>
2023-04-24 12:53:43 +0300

    Update registration.php

---
+++ registration.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Регистрация</title>
+</head>
+<body>
+    <h1>Регистрация</h1>
+    <form class="aa" method='post'>
+        <input type="text" name='login' placeholder='Логин'>
+        <input type="password" name='password' placeholder='Пароль'>
+        <button type="submit">Зарегистрироваться</button>
+    </form>
+</body>
+</html>
